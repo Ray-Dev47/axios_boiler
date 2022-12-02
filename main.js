0ac8d91c739e1178144b09f89f0f85469f45821f
Raymond Idengeli <ray.idengeli@gmail.com>
2022-12-02 23:03:09 +0100

    axios interceptors fix

--- main.js
+++ main.js
@@ -2,6 +2,18 @@
 
 // const { default: axios } = require("axios");
 
+
+// AXIOS GLOBALS
+// if we have encrypted routes and we want to pass like bearer/tokens, authorization , rather than enter constom header config for every endpoint we use a global method below:
+axios.defaults.headers.post["Content-Type"] = "application/json";
+axios.defaults.headers.common['Authorization'] = "some AUTH_TOKEN here"; 
+// axios.defaults.headers.common['Authorization'] = "https://jwt.io/#debugger-io?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlJheW1vbmQgSWRlbmdlbGkiLCJpYXQiOjE1MTYyMzkwMjJ9.vfkaK-q5QoJeMLD3gb80GJlI59V51ONA8tkHSLQr60Q";
+
+// headers: {
+// 	'Content-type': "application/json",
+// 	Authorization: 'Some token here'
+// }
+
 // GET REQUEST
 // method one
 function getTodos() {
@@ -41,8 +53,7 @@ function addTodo() {
 
 	// method 2
 	// axios.post('https://jsonplaceholder.typicode.com/posts')
-	axios
-		.post('https://jsonplaceholder.typicode.com/posts', {
+	axios.post('https://jsonplaceholder.typicode.com/posts', {
 			userId: 1,
 			id: 1,
 			title: 'Ray',
@@ -106,26 +117,57 @@ function removeTodo() {
 	// 	.then((res) => showOutput(res))
 	// 	.catch((err) => console.error(err));
 
-	// 2
-	axios.delete('https://jsonplaceholder.typicode.com/posts/1', {
-		id: 1,
-		title: 'deleted title',
-		body: 'deleted body',
-	}).then((res) => showOutput(res))
-    .catch((err) => console.log(err));
+	//method 2
+	axios
+		.delete('https://jsonplaceholder.typicode.com/posts/1', {
+			id: 1,
+			title: 'deleted title',
+			body: 'deleted body',
+		})
+		.then((res) => showOutput(res))
+		.catch((err) => console.log(err));
 }
 
-// SIMULTANEOUS DATA
+// SIMULTANEOUS DATA  -- e.g when we try to perform get, post , delete at the same time,
+// to achieve this, we use axios.all
 function getData() {
 	console.log('Simultaneous Request');
+	axios
+		.all([
+			axios.get('https://jsonplaceholder.typicode.com/todos?_limit=3'),
+			axios.get('https://jsonplaceholder.typicode.com/posts?_limit=3'),
+		])
+		// .then(res => {
+		//     console.log(res[0])
+		//     console.log(res[1])
+		//     showOutput(res[1])
+		// })
+		// cleaner method
+		.then(axios.spread((todos, posts) => showOutput(posts)))
+		.catch((err) => console.log(err));
 }
 
-// CUSTOM HEADERS
+// CUSTOM HEADERS - allows us customize headers like in the case of logins or tokens 
 function customHeaders() {
 	console.log('Custom Headers');
+	let config = {
+		headers: {
+			'Content-type': "application/json",
+			Authorization: 'Some token here'
+		}
+	}
+	axios.post('https://jsonplaceholder.typicode.com/posts', {
+			userId: 1,
+			id: 1,
+			title: 'Ray',
+			body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+		}, config)
+		.then((res) => showOutput(res))
+		.catch((err) => console.log(err, 'error'));
+
 }
 
-// TRANSFORMING REQUESTS & RESPONSES
+// TRANSFORMING REQUESTS & RESPONSES -allows us transorm requests in certain ways (learn later)
 function transformResponse() {
 	console.log('Transform Response');
 }
@@ -140,7 +182,24 @@ function cancelToken() {
 	console.log('Cancel Token');
 }
 
-// INTERCEPTING REQUESTS & RESPONSES
+// INTERCEPTING REQUESTS & RESPONSES - allows us to run special(any) functions before the request/response runs
+axios.interceptors.request.use(
+	(config) => {
+		console.log(
+			`**Testing interceptors ${config.method.toUpperCase()} request sent to ${
+				config.url
+			} at ${new Date().getTime()}`
+		);
+		return config;
+	},
+	// function (error) {
+	//     // Do something with request error
+	//     return Promise.reject(error);
+	//   }
+	(error) => {
+		return Promise.reject(error);
+	}
+);
 
 // AXIOS INSTANCES
 
